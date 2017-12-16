bb0477aaa800247113d4b5cb9abd52f695d75750
Juanita Hansen <jjhansen@gmail.com>
2017-12-16 10:36:09 -0800

    Fix issue where messages weren't resolving when app is packaged
    Missing @Configuration annotation on ServletConfig class caused message bundle to not be loaded when run as a packaged war file

--- hueapp/src/main/java/com/electromagneticsoftware/configuration/ServletConfig.java
+++ hueapp/src/main/java/com/electromagneticsoftware/configuration/ServletConfig.java
@@ -1,10 +1,12 @@
 package com.electromagneticsoftware.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class ServletConfig extends WebMvcConfigurerAdapter {
     /**
      *  Message externalization/internationalization
@@ -15,7 +17,7 @@ public class ServletConfig extends WebMvcConfigurerAdapter {
         resourceBundleMessageSource.setBasename("Messages");
         return resourceBundleMessageSource;
     }
-
+    
     /* ******************************************************************* */
     /*  Defines callback methods to customize the Java-based configuration */
     /*  for Spring MVC enabled via {@code @EnableWebMvc}                   */
@@ -24,11 +26,11 @@ public class ServletConfig extends WebMvcConfigurerAdapter {
     /**
      *  Dispatcher configuration for serving static resources
      */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+//    }
 
 
 }
